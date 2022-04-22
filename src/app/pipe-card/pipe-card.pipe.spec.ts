da49b6908706bcdac95fa33ebc32f88c5780ea91
AracelyCondoriTipola <aracely96.ac>
2022-04-21 23:29:35 -0400

    corrigiendo

--- src/app/pipe-card/pipe-card.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeCardPipe } from './pipe-card.pipe';
-
-describe('PipeCardPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeCardPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
