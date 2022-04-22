bd8ed0b0b1d14ce75410c5b8422558d766f672ac
mayu nina <mayumi>
2022-04-21 22:09:06 -0400

    creando archivos app

--- src/app/pipes Precio/filterPrecio.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterPipe2 } from './filterPrecio.pipe';
-
-describe('FilterPipe2', () => {
-  it('create an instance', () => {
-    // const pipe = new FilterPipe1();
-    // expect(pipe).toBeTruthy();
-  });
-});
