8050599850e48cb3c24b3ac0e86b8c3e42ec6ca9
AracelyCondoriTipola <aracely96.ac>
2022-04-22 01:55:43 -0400

    agregando componentes

--- MODAStore_frontend/src/app/src/app/modules/auth/login/login.component.spec.ts
+++ MODAStore_frontend/src/app/src/app/modules/auth/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModulesComponent } from './modules.component';
+import { LoginComponent } from './login.component';
 
-describe('ModulesComponent', () => {
-  let component: ModulesComponent;
-  let fixture: ComponentFixture<ModulesComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModulesComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ModulesComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
