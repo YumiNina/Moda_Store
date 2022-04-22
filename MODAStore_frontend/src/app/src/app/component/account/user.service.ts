d76ba0f3e6a7580adc2c00b5190ca10272300729
mayu nina <mayumi>
2022-04-22 11:02:04 -0400

    modificando el puerto de user

--- MODAStore_frontend/src/app/src/app/component/account/user.service.ts
+++ MODAStore_frontend/src/app/src/app/component/account/user.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { User} from './user';
+import { User } from './account-info/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService{
-  private url:string="http://localhost:8081/user";
+  private url:string="http://localhost:8083/user";
 
   constructor(private http:HttpClient){}
 
