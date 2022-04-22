0f4be7fadc2c50284d0e4f14306809d4a3fa4ef3
mayu nina <mayumi>
2022-04-22 11:12:45 -0400

    modificando el puerto de product-detalle

--- MODAStore_frontend/src/app/src/app/component/product-details/product.service.ts
+++ MODAStore_frontend/src/app/src/app/component/product-details/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from './product';
 })
 export class ProductService {
 
-  private url:string="http://localhost:8081/producto";
+  private url:string="http://localhost:8083/producto";
 
   constructor(private http:HttpClient) { }
 
