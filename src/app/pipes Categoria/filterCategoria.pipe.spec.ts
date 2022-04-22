5a3757b79854fd62268a3fb41e28d17f454519ef
mayu nina <mayumi>
2022-04-21 22:33:43 -0400

    modificando carpeta pipes categoria

--- src/app/pipes Categoria/filterCategoria.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterPipe1 } from './filterCategoria.pipe';
-
-describe('FilterPipe1', () => {
-  it('create an instance', () => {
-    // const pipe = new FilterPipe1();
-    // expect(pipe).toBeTruthy();
-  });
-});
