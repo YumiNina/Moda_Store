f1d88c4fd9ca10630be3384c1c7b8d457aa77ea6
nina mayumi <mayunina55@gmail.com>
2022-05-16 09:42:45 -0400

    agregando components

--- MODAStore_frontend/src/app/app-routing.module.ts
+++ MODAStore_frontend/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { CategoriasComponent } from './component/categorias/categorias.component
 import { FormCategoriaComponent } from './component/categorias/form-categoria.component';
 import { ClientEditComponent } from './component/client-edit/client-edit.component';
 import { ListClientsComponent } from './component/list-clients/list-clients.component';
+import { MostBougthComponent } from './component/most-bougth/most-bougth.component';
 import { PedidosComponent } from './component/pedidos/pedidos.component';
 import { PedirComponent } from './component/pedir/pedir.component';
 import { ProbadorVirtualComponent } from './component/probador-virtual/probador-virtual.component';
