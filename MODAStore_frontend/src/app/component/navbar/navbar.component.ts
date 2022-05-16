d365a55c632ffd13b949707bef16b3234715b0ab
nina mayumi <mayunina55@gmail.com>
2022-05-16 10:03:13 -0400

    agregando components

--- MODAStore_frontend/src/app/component/navbar/navbar.component.ts
+++ MODAStore_frontend/src/app/component/navbar/navbar.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
+
+
+  styleUrls: ['./navbar.component.css'],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
