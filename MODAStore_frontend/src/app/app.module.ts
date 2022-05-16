a0950ea6539b70d3782e67f2a697e4f3400b5238
nina mayumi <mayunina55@gmail.com>
2022-05-16 13:56:56 -0400

    agregando los modulos

--- MODAStore_frontend/src/app/app.module.ts
+++ MODAStore_frontend/src/app/app.module.ts
@@ -13,6 +13,40 @@ import { PipeCardPipe } from './pipe-card/pipe-card.pipe';
 import { MainComponent } from './main/main.component';
 import { AuthModule } from './modules/auth/auth.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountInfoComponent } from './component/account/account-info/account-info.component';
+import { AccountUpdateComponent } from './component/account/account-update/account-update.component';
+import { CardUpdateComponent } from './component/card-update/card-update.component';
+import { CardFormComponent } from './component/card/card-form.component';
+import { CardComponent } from './component/card/card.component';
+import { CartComponent } from './component/cart/cart.component';
+import { CatalogueComponent } from './component/catalogue/catalogue.component';
+import { CategoriasComponent } from './component/categorias/categorias.component';
+import { FormCategoriaComponent } from './component/categorias/form-categoria.component';
+import { ClientEditComponent } from './component/client-edit/client-edit.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { ListClientsComponent } from './component/list-clients/list-clients.component';
+import { MostBougthComponent } from './component/most-bougth/most-bougth.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { PedidosAdminComponent } from './component/pedidos-admin/pedidos-admin.component';
+import { PedidosComponent } from './component/pedidos/pedidos.component';
+import { PedirComponent } from './component/pedir/pedir.component';
+import { ProbadorVirtualComponent } from './component/probador-virtual/probador-virtual.component';
+import { ProductBuyComponent } from './component/product-buy/product-buy.component';
+import { ProductDetailsComponent } from './component/product-details/product-details.component';
+import { FormProductComponent } from './component/products/form-product.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ProfileClientComponent } from './component/profile-client/profile-client.component';
+import { ProfileViewComponent } from './component/profile-view/profile-view.component';
+import { ReceiptComponent } from './component/receipt/receipt.component';
+import { LessOrderComponent } from './component/reports/less-order/less-order.component';
+import { MoreOrderComponent } from './component/reports/more-order/more-order.component';
+import { SesionComponent } from './component/sesion/sesion.component';
+import { UserAdminUpdateComponent } from './component/user/user-admin-update/user-admin-update.component';
+import { UserAdminComponent } from './component/user/user-admin/user-admin.component';
+import { UserFormComponent } from './component/user/user-admin/user-form.component';
+import { UserLoginComponent } from './component/user/user-login/user-login.component';
+import { UserUpdateComponent } from './component/user/user-update/user-update.component';
 
 const routes: Routes = [
   { path: 'sesion', component: SesionComponent  },
@@ -65,11 +99,6 @@ const routes: Routes = [
   { path: 'account-info/:id', component: AccountInfoComponent, pathMatch:'full'},
   { path: 'account-update', component: AccountUpdateComponent, pathMatch: 'full'},
   { path: 'account-update/:id', component: AccountUpdateComponent, pathMatch: 'full'},
-
-
-
-
-
   { path: '', redirectTo: '/', pathMatch: 'full' },
 ];
 
