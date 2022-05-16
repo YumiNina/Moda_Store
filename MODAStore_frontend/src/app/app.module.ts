346b764165579bcb879744c03db75a91cdfbbfd1
nina mayumi <mayunina55@gmail.com>
2022-05-16 10:08:26 -0400

    agregando correcciones de path

--- MODAStore_frontend/src/app/app.module.ts
+++ MODAStore_frontend/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SesionComponent } from './component/sesion/sesion.component';
 
 
 
